ccb06b0ead0b0f60cec06141fb437840770a491f
Jabiyeff <cebiyevanar@gmail.com>
2023-04-29 11:13:37 +0400

    spes: Update blobs from SPESGlobal V13.0.12.0-A12
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- proprietary/vendor/radio/qcril_database/upgrade/16_version_update_ecc_table.sql
+++ proprietary/vendor/radio/qcril_database/upgrade/16_version_update_ecc_table.sql
@@ -8,29 +8,12 @@ BEGIN TRANSACTION;
 
 INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 16);
 
-DELETE FROM qcril_emergency_source_mcc_mnc_table   where MCC = '722' AND MNC = '07' ;
-DELETE FROM qcril_emergency_source_voice_mcc_mnc_table   where MCC = '722' AND MNC = '07' ;
+
 DELETE FROM qcril_emergency_source_mcc_mnc_table   where MCC = '340' AND MNC = '01' ;
 DELETE FROM qcril_emergency_source_mcc_mnc_table   where MCC = '647' AND MNC = '00' ;
 DELETE FROM qcril_emergency_source_mcc_mnc_table   where MCC = '647' AND MNC = '00' ;
-
 DELETE FROM qcril_emergency_source_mcc_table   where MCC = '208' ;
 DELETE FROM qcril_emergency_source_voice_table   where MCC = '208' ;
-
-
-
-
-
-INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('722','07','100','','');
-INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('722','07','102','','');
-INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('722','07','105','','');
-INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('722','07','106','','');
-INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('722','07','107','','');
-INSERT INTO "qcril_emergency_source_voice_mcc_mnc_table" VALUES('722','07','100','','');
-INSERT INTO "qcril_emergency_source_voice_mcc_mnc_table" VALUES('722','07','102','','');
-INSERT INTO "qcril_emergency_source_voice_mcc_mnc_table" VALUES('722','07','105','','');
-INSERT INTO "qcril_emergency_source_voice_mcc_mnc_table" VALUES('722','07','106','','');
-INSERT INTO "qcril_emergency_source_voice_mcc_mnc_table" VALUES('722','07','107','','');
 INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('340','01','112','','');
 INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('340','01','911','','');
 INSERT INTO "qcril_emergency_source_mcc_mnc_table" VALUES('647','00','112','','');
--- proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
+++ proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
@@ -8,11 +8,4 @@ BEGIN TRANSACTION;
 
 INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 7);
 
-DELETE FROM qcril_emergency_source_hard_mcc_table where MCC = '722'  AND NUMBER = '100';
-DELETE FROM qcril_emergency_source_hard_mcc_table where MCC = '722'  AND NUMBER = '107';
-
-
-INSERT INTO "qcril_emergency_source_hard_mcc_table" VALUES('722','100','','');
-INSERT INTO "qcril_emergency_source_hard_mcc_table" VALUES('722','107','','');
-
 COMMIT TRANSACTION;
