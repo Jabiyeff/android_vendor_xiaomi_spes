5da8a6d3f7c8dfdba002590bbceff74524453348
angelomds42 <angelomds42@gmail.com>
2025-05-02 23:16:15 +0400

    spes: Drop stock offline charger daemon - There is no reason to have problems with missing symbols with every Android update when we can use the aosp charger
    Signed-off-by: angelomds42 <angelomds42@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- spes-vendor.mk
+++ spes-vendor.mk
@@ -1145,7 +1145,6 @@ PRODUCT_PACKAGES += \
     manifest_vendor.xiaomi.hardware.mlipay.xml \
     manifest_vendor.xiaomi.hardware.mtdservice.xml \
     vendor.qti.gnss@4.0-service.xml \
-    chargeonlymode \
     ATFWD-daemon \
     STFlashTool \
     adpl \
